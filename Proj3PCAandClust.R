464ebf3276f6c0346217bf2c6489cc5a8872443d
Ben Graves <blgpp5@mail.missouri.edu>
2021-12-07 20:19:29 -0600

    gets cluster k search working, starts up KPCA

--- Proj3PCAandClust.R
+++ Proj3PCAandClust.R
@@ -1,7 +1,7 @@
 ## PCA FILE
 
 ## PCA packages
-ibrary(readr)
+library(readr)
 library(ggplot2)
 library(kernlab)
 library(NMF)
@@ -31,11 +31,11 @@ LongLat <- SST_df[, 1:2]
 MonthlySST <- SST_df[ , -c(1,2)]
 
 ## Center data
-SSTgrand <- mean(MonthlySST)
-MonthlySST_cent <- MonthlySST - SSTgrand
+# SSTgrand <- mean(MonthlySST)
+# MonthlySST_cent <- MonthlySST - SSTgrand
 
 ## Start with SVD to get an idea of Prin Comp needed
-SSTSVD <- svd(MonthlySST_cent)
+SSTSVD <- svd(MonthlySST)
 SSTsingvals <- matrix(NA, nrow = 842, ncol = 2)
 SSTsingvals[, 1] <- 1:842
 SSTsingvals[, 2] <- SSTSVD$d
@@ -46,7 +46,7 @@ cumsum(SSTSVD$d[1:33]^2)/sum(SSTSVD$d^2)*100
 set.seed(938837)
 rand <- matrix(NA, 2512, 842)
 for (i in 1:842) {
-    rand[ , i] <- MonthlySST_cent[sample(nrow(MonthlySST_cent)), i]
+    rand[ , i] <- MonthlySST[sample(nrow(MonthlySST)), i]
 }
 
 randdecomp <- svd(rand)
@@ -60,10 +60,35 @@ legend(100, 500, c('Real Trace', 'Random Trace'), fill = c('black', 'blue'))
 ## PC DF
 SSTUD <- SSTSVD$u %*% diag(SSTSVD$d)
 SSTUD <- cbind(LongLat, SSTUD[ , -c(34:842)])
+colnames(SSTUD) <- c('Long', 'Lat', paste0('PC', 1:33))
 
 ## Plot PC 1
-SST_raster <- rasterFromXYZ(SSTUD[, c(1, 2, 3)])
+PC1_raster <- rasterFromXYZ(SSTUD[, c(1, 2, 3)])
 plot(SST_raster)
 
 ## Cluster with SVD
-clustres <- kmeans(SSTUD[, -c(1, 2)], 3, nstart = 20)
+
+## Options for assessing number of clusters
+set.seed(662321)
+SVDbestK <- NbClust(SSTUD[, -c(1,2)], method = 'kmeans',  min.nc = 2, max.nc = 20, index = "all")
+par(mfrow = c(1,1))
+SVDbestK2 <- sapply(1:20, 
+                    function(k){kmeans(SSTUD[, -c(1,2)], k, nstart=50,iter.max = 15 )$tot.withinss})
+SVDclustres <- kmeans(SSTUD[, -c(1, 2)], 2, nstart = 20, iter.max = 15)
+SVDclustout <- factor(SVDclustres$cluster)
+
+SVDclust_raster <- rasterFromXYZ(cbind(LongLat, SVDclustout))
+plot(SVDclust_raster)
+
+form <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), 
+              panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+baseplot <- ggplot(data = data.frame(SSTUD, SVDclustout), aes(x = PC1, y = PC2, color = SVDclustout))
+
+baseplot + geom_point() + form + xlab('Principal Component 1') + 
+           ylab('Principal Component 2') + ggtitle('Plot of Clustering for First Two PCs')
+
+## KPCA
+kpca1 <- kpca(MonthlySST, kernel = "rbfdot", kpar = list(sigma = .03)) ## maybe change up sigma but doesn't seem good
+kpca2 <- kpca(MonthlySST, kernel = "vanilladot", kpar = list()) ##maybe 3-6 PCs?
+kpca3 <- kpca(MonthlySST, kernel = "laplacedot", kpar = list(sigma = .05)) ##9 - 14 PCs?
