4c991891fe89e2f818395c11f710f467d2e79245
Ben Graves <blgpp5@mail.missouri.edu>
2021-12-08 13:53:03 -0600

    updates PCA

--- Proj3PCAandClust.R
+++ Proj3PCAandClust.R
@@ -12,7 +12,7 @@ library(RSpectra)
 library(maps)
 library(fields)
 library(raster) 
-
+library(NbClust)
 
 ## Call Data
 load("data_in_dfs.RData")
@@ -74,6 +74,7 @@ SVDbestK <- NbClust(SSTUD[, -c(1,2)], method = 'kmeans',  min.nc = 2, max.nc = 2
 par(mfrow = c(1,1))
 SVDbestK2 <- sapply(1:20, 
                     function(k){kmeans(SSTUD[, -c(1,2)], k, nstart=50,iter.max = 15 )$tot.withinss})
+set.seed(662321)
 SVDclustres <- kmeans(SSTUD[, -c(1, 2)], 2, nstart = 20, iter.max = 15)
 SVDclustout <- factor(SVDclustres$cluster)
 
@@ -90,5 +91,56 @@ baseplot + geom_point() + form + xlab('Principal Component 1') +
 
 ## KPCA
 kpca1 <- kpca(MonthlySST, kernel = "rbfdot", kpar = list(sigma = .03)) ## maybe change up sigma but doesn't seem good
-kpca2 <- kpca(MonthlySST, kernel = "vanilladot", kpar = list()) ##maybe 3-6 PCs?
-kpca3 <- kpca(MonthlySST, kernel = "laplacedot", kpar = list(sigma = .05)) ##9 - 14 PCs?
+kpca2 <- kpca(MonthlySST, kernel = "vanilladot", kpar = list()) ##maybe 3-6 PCs? Better than Random suggests 32
+kpca3 <- kpca(MonthlySST, kernel = "laplacedot", kpar = list(sigma = .03)) ##9 - 14 PCs?
+
+kpcarand <- kpca(rand, kernel = "vanilladot", kpar = list())
+
+KPCApcs <- kpca2@pcv[ , 1:6]
+
+## Cluster for KPCA
+set.seed(662321)
+KPCAbestK <- NbClust(KPCApcs, method = 'kmeans',  min.nc = 2, max.nc = 20, index = "all")
+par(mfrow = c(1,1))
+KPCAbestK2 <- sapply(1:20, 
+                    function(k){kmeans(KPCApcs, k, nstart=50,iter.max = 15 )$tot.withinss})
+set.seed(662321)
+KPCAclustres <- kmeans(KPCApcs, 5, nstart = 20, iter.max = 15)
+KPCAclustout <- factor(KPCAclustres$cluster)
+
+KPCAclust_raster <- rasterFromXYZ(cbind(LongLat, KPCAclustout))
+plot(KPCAclust_raster)
+
+
+baseplot <- ggplot(data = data.frame(KPCApcs, KPCAclustout), aes(x = KPCApcs[, 1], y = KPCApcs[, 2], color = KPCAclustout))
+
+baseplot + geom_point() + form + xlab('Principal Component 1') + 
+    ylab('Principal Component 2') + ggtitle('Plot of KPCA Clustering for First Two PCs')
+
+
+## LLE
+## Probably have to turn off parallel if using Windows, takes ages even with parallel
+kopt <- calc_k(MonthlySST, m = 2, kmin = 15, kmax = 30, parallel = TRUE, cpus = 3)
+
+## Opt K = 16 for 12, K = 15 for 2, not really sure how to pick m
+lleres <- lle(MonthlySST, m = 2, k = 16)
+Ymat <- data.frame(lleres$Y)
+
+## Cluster for LLE
+set.seed(662321)
+LLEbestK <- NbClust(Ymat, method = 'kmeans',  min.nc = 2, max.nc = 20, index = "all")
+par(mfrow = c(1,1))
+LLEbestK2 <- sapply(1:20, 
+                     function(k){kmeans(Ymat, k, nstart=50,iter.max = 15 )$tot.withinss})
+set.seed(662321)
+LLEclustres <- kmeans(Ymat, 10, nstart = 20, iter.max = 15)
+LLEclustout <- factor(LLEclustres$cluster)
+
+LLEclust_raster <- rasterFromXYZ(cbind(LongLat, LLEclustout))
+plot(LLEclust_raster)
+
+
+baseplot <- ggplot(data = data.frame(LLEpcs, LLEclustout), aes(x = LLEpcs[, 1], y = LLEpcs[, 2], color = LLEclustout))
+
+baseplot + geom_point() + form + xlab('Principal Component 1') + 
+    ylab('Principal Component 2') + ggtitle('Plot of LLE Clustering for First Two PCs')
