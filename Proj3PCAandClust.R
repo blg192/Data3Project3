6dc18be94b53d959c9804bbbe8ca76a99d811107
Ben Graves <blgpp5@mail.missouri.edu>
2021-12-10 18:34:59 -0600

    tries some data reformatting

--- Proj3PCAandClust.R
+++ Proj3PCAandClust.R
@@ -318,3 +318,16 @@ baseplot + geom_point() + form + xlab('Principal Component 1') +
 
 
 ## I think KPCA might be the best options for both data sets
+
+## Adding clusters to data frames and reformatting 
+
+## Long Format SST
+SST_df_clusts <- data.frame(SST_df, Clusters = KPCAclustout)
+long_SST <- melt(SST_df_clusts, id.vars = colnames(SST_df_clusts[, c(1, 2, 845)]))
+colnames(long_SST) <- c('Longitude', 'Latitude', 'Cluster', 'Date', 'Temperature')
+
+## Long Format Precip
+Pdat_df_clusts <- data.frame(Pdat_df, Clusters = PKPCAclustout)
+long_Pdat <- melt(Pdat_df_clusts, id.vars = colnames(SST_df_clusts[, c(1, 2, 845)]))
+colnames(long_Pdat) <- c('Longitude', 'Latitude', 'Cluster', 'Date', 'Precipitation')
+
