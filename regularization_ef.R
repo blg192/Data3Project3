45636d23c6b2ae1f6065561cdb767cb1b5f75e99
Ellen Fitzsimmons <eakfitzsimmons@gmail.com>
2021-12-14 23:47:58 -0600

    rf lag code

--- regularization_ef.R
+++ regularization_ef.R
@@ -220,6 +220,37 @@ abline(0,1)
 mean((yhat.rf - SST_df_l[subte, 4])^2)
 
 
+
+
+
+# load 
+load("SST_clustwide.RData")
+load("Pdat_clustwide.RData")
+load("SST_longagg.RData")
+load("Pdat_longagg.RData")
+load("Pdat_clus.RData")
+
+## row 829 is beginning of 2017
+train <- c(1:828)
+
+all_train <- data.frame(Pdat_clustwide[train,], SST_clustwide[train,])
+all_long_train <- gather(all_train, landclus, landclusavg, 2:13)
+all_long_train <- all_long_train[-2]
+
+all_test <- data.frame(Pdat_clustwide[-train,], SST_clustwide[-train,])
+all_long_test <- gather(all_test, landclus, landclusavg, 2:13)
+all_long_test <- all_long_test[-2]
+
+all <- data.frame(Pdat_clustwide, SST_clustwide)
+all_long <- gather(all, landclus, landclusavg, 2:13)
+all_long <- all_long[-2]
+## split by cluster
+all_long <- split(all_long, all_long$landclus)
+
+save(all_long, file = "all_long.RData")
+load("all_long.RData")
+
+
 ## using sst and precip
 set.seed(125498)
 m <- round(sqrt(ncol(all_long_train) - 2), 0)
@@ -237,7 +268,7 @@ plot(yhat.rf, all_long_test[, 8])
 abline(0,1)
 testmse <- mean((yhat.rf - all_long_test[, 8])^2)
 
-load("Pdat_df_l.RData")
+# load("Pdat_df_l.RData")
 precip_yyhat <- data.frame(all_long_test[, c(1, 7:8)], "yhat" = yhat.rf)
 for(i in 1:nrow(precip_yyhat)){
   if(precip_yyhat$landclus[i] == "LandCluster1"){
@@ -279,6 +310,141 @@ save(all_precip_yyhat, file = "all_precip_yyhat.RData")
 
 load("all_precip_yyhat.RData")
 testmse_rf <- mean((all_precip_yyhat$yhat - all_precip_yyhat$Precipitation)^2)
+
+
+
+## Now do random forest with lagged data
+## lag the sst data
+## set tau to 6 months
+# all <- data.frame(Pdat_clustwide, SST_clustwide)
+# all_long <- gather(all, landclus, landclusavg, 2:13)
+# all_long <- all_long[-2]
+# save(all_long, file = "all_long.RData")
+load("all_long.RData")
+
+
+lagdata <- function(df, tau){
+  ## df is dataframe
+  ## tau is distance from prediction point
+  mat <- as.matrix(df)
+  lag <- matrix(NA, nrow = nrow(mat) + tau, ncol = ncol(mat))
+  colnames(lag) <- c(colnames(mat))
+  for(i in 1:nrow(lag)){
+    if(i < (tau+1)){
+      lag[i, 1] <- mat[i, 1]
+      lag[i, 2:6] <- c(rep(NA, 5))
+      lag[i, 7:8] <- mat[i, 7:8]
+    } else if(i < (nrow(mat) + 1)){
+      lag[i, 1] <- mat[i, 1]
+      lag[i, 2:6] <- mat[(i - tau), 2:6]
+      lag[i, 7:8] <- mat[i, 7:8]
+    } else{
+      lag[i, 1] <- NA
+      lag[i, 2:6] <- mat[(i - tau), 2:6]
+      lag[i, 7:8] <- c(NA, 2)
+    }
+  }
+  lagdf <- as.data.frame(lag)
+  return(lagdf)
+}
+
+## lagged data when tau = 6 months
+tau <- 6
+lagdf <- vector("list", length = 12)
+train <- vector("list", length = 12)
+rf_lag <- vector("list", length = 12)
+yhat.rflag <- vector("list", length = 12)
+testmse <- vector("list", length = 12)
+precip_yyhat_lag <- vector("list", length = 12)
+for(i in 1:12){
+  lagdf[[i]] <- lagdata(all_long[[i]], tau)
+  ## remove top and bottom tau rows with NAs
+  lagdf[[i]] <- lagdf[[i]][(tau + 1):nrow(all_long[[i]]),]
+  
+  ## need to switch to factor, numeric, etc
+  lagdf[[i]]$SeaCluster1 <- as.numeric(lagdf[[i]]$SeaCluster1)
+  lagdf[[i]]$SeaCluster2 <- as.numeric(lagdf[[i]]$SeaCluster2)
+  lagdf[[i]]$SeaCluster3 <- as.numeric(lagdf[[i]]$SeaCluster3)
+  lagdf[[i]]$SeaCluster4 <- as.numeric(lagdf[[i]]$SeaCluster4)
+  lagdf[[i]]$SeaCluster5 <- as.numeric(lagdf[[i]]$SeaCluster5)
+  lagdf[[i]]$landclusavg <- as.numeric(lagdf[[i]]$landclusavg)
+  
+  ## split lag data into train and test
+  ## row 823 is beginning of 2017
+  train[[i]] <- c(1:max(which(lagdf[[i]]$Date == "Dec2016")))
+  
+  ## do everything else again
+  ## using lagged sst and precip
+  set.seed(125498)
+  m <- round(sqrt(ncol(lagdf[[i]]) - 2), 0)
+  rf_lag[[i]] <- randomForest(landclusavg ~ SeaCluster1 + SeaCluster2 + SeaCluster3 + SeaCluster4 + SeaCluster5,
+                         data = lagdf[[i]][train[[i]],],
+                         mtry = m,
+                         importance = TRUE)
+  
+  yhat.rflag[[i]] <- predict(rf_lag[[i]], newdata = as.matrix(lagdf[[i]][-train[[i]], c(-1, -7)]))
+  
+  testmse[[i]] <- mean((yhat.rflag[[i]] - lagdf[[i]][-train[[i]], 8])^2)
+  
+  precip_yyhat_lag[[i]] <- data.frame(lagdf[[i]][-train[[i]], c(1, 7:8)], "yhat" = yhat.rflag[[i]])
+  for(j in 1:nrow(precip_yyhat_lag[[i]])){
+    if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster1"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 1
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster2"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 2
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster3"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 3
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster4"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 4
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster5"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 5
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster6"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 6
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster7"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 7
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster8"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 8
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster9"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 9
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster10"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 10
+    } else if(precip_yyhat_lag[[i]]$landclus[j] == "LandCluster11"){
+      precip_yyhat_lag[[i]]$landclus[j] <- 11
+    } else{
+      precip_yyhat_lag[[i]]$landclus[j] <- 12
+    }
+  }
+  precip_yyhat_lag[[i]]$landclus <- as.factor(precip_yyhat_lag[[i]]$landclus)
+  
+}
+
+## mse with lagged data
+testmse <- mean(unlist(testmse))
+
+## need to set i for importance and plot
+# rf_lag[[i]]$importance
+# plot(yhat.rflag, lagdf[-train, 8])
+# abline(0,1)
+
+precip_yyhat_lag2 <- rbind.data.frame(precip_yyhat_lag[[1]], precip_yyhat_lag[[2]],
+                                      precip_yyhat_lag[[3]], precip_yyhat_lag[[4]],
+                                      precip_yyhat_lag[[5]], precip_yyhat_lag[[6]],
+                                      precip_yyhat_lag[[7]], precip_yyhat_lag[[8]],
+                                      precip_yyhat_lag[[9]], precip_yyhat_lag[[10]],
+                                      precip_yyhat_lag[[11]], precip_yyhat_lag[[12]])
+
+## limit lagdf to the test observations
+all_yyhat_lag <- merge.data.frame(long_Pdat_test, precip_yyhat_lag2, 
+                                     by.x = c(4, 3), by.y = 1:2)
+
+## save data
+save(all_yyhat_lag, file = "all_yyhat_lag.RData")
+
+load("all_yyhat_lag.RData")
+testmse_rflag <- mean((all_yyhat_lag$yhat - all_yyhat_lag$Precipitation)^2)
+
+
+
 
 #################################### BART #################################
 
